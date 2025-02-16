41eec6257a5280fab3d3c74d3e4a2d213ba98030
kalanaSNK <kalanaSNK1@gmail.com>
2025-02-16 13:35:04 +0530

    add table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import { Book } from "./components/Book";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Book/>
     </div>
   );
 }
